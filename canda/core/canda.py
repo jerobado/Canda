6dfa2af4f669a3813b338c953c5ff29889bfdc88
Jero Bado <tokidokitalkyou@gmail.com>
2019-09-19 19:52:46 +0800

    Created canda.py
    This will be the core operation of Canda that will power the whole GUI. Just separating the back-end from the front-end.

---
+++ canda/core/canda.py
@@ -0,0 +1,14 @@
+""" Core operation of Canda """
+
+
+def _master_key():
+
+    # [] TODO: think how and where to retrieve the masterkey in a secure place
+
+    return 'masterkey'
+
+
+MASTER_KEY = _master_key()
+
+
+
