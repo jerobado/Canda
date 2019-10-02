6ec5c779b0fb1787f5ce63648654a389f5fefb2a
Jero Bado <tokidokitalkyou@gmail.com>
2019-10-02 20:15:43 +0800

    Created sample data in the listwidget and textedit
    Use string.Template class to properly display the selected record.

--- canda/data/constant.py
+++ canda/data/constant.py
@@ -1,4 +1,15 @@
 
-RECORDS = ['pixie@gmail.com',
-           'hungrypoet_32@yahoo.com',
-           'blandsword@microsoft.com']
+from string import Template
+
+_template = """Account: $account
+Username: $username
+Password: $password
+"""
+
+DETAILS_TEMPLATE = Template(_template)
+
+
+RECORDS = [{'username': 'pixie@gmail.com', 'password': 'helloworld1234', 'account': 'GMail'},
+           {'username': 'hungrypoet_32@yahoo.com', 'password': 'novels111', 'account': 'Yahoo'},
+           {'username': 'blandsword@microsoft.com', 'password': 'helloworld1234', 'account': 'Microsoft'}]
+
