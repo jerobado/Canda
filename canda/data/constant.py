10099899fd5668c29bd59e0db3d070997e43203e
Jero Bado <tokidokitalkyou@gmail.com>
2019-10-15 20:14:11 +0800

    Removed default values when the application initially starts

--- canda/data/constant.py
+++ canda/data/constant.py
@@ -7,9 +7,4 @@ Password: $password
 """
 
 DETAILS_TEMPLATE = Template(_template)
-
-
-RECORDS = [{'username': 'pixie@gmail.com', 'password': 'helloworld1234', 'account': 'GMail'},
-           {'username': 'hungrypoet_32@yahoo.com', 'password': 'novels111', 'account': 'Yahoo'},
-           {'username': 'blandsword@microsoft.com', 'password': 'helloworld1234', 'account': 'Microsoft'}]
-
+RECORDS = []
