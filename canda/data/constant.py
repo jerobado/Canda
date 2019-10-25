193c21d5e4bf267f2111e21c7a3b2939172b93df
Jero Bado <tokidokitalkyou@gmail.com>
2019-10-25 20:14:00 +0800

    Removed environment variable debug messages

--- canda/data/constant.py
+++ canda/data/constant.py
@@ -13,6 +13,3 @@ RECORDS = []
 SALT = base64.b64decode(os.environ['CANDA_SALT'])
 LOGIN_TOKEN = os.environ['CANDA_LOGIN_TOKEN'].encode()
 LOGIN_VERIFICATION_MESSAGE = b'password verified'
-
-print(f'SALT: {SALT}')
-print(f'LOGIN_TOKEN: {LOGIN_TOKEN}')
